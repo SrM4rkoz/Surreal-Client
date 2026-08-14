f183efb31c1f5d6b4667b4a806bb2413ac9b5b86
Sr M4rkoz <srm4rkoz@gmail.com>
2026-08-14 19:25:35 -0300

    Adicionado o SVAR - DataGrid

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { invoke } from "@tauri-apps/api/core";
+  import { Grid, Willow } from "@svar-ui/svelte-grid";
 
   let name = $state("asdff");
   let greetMsg = $state("");
@@ -9,6 +10,61 @@
     // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
     greetMsg = await invoke("greet", { name });
   }
+
+  // GRID:
+  const data = [
+    {
+      id: 1,
+      city: "Amieshire",
+      email: "Leora13@yahoo.com",
+      firstName: "Ernest",
+      lastName: "Schuppe",
+      companyName: "Lebsack - Nicolas",
+    },
+    {
+      id: 2,
+      city: "Gust",
+      email: "Mose_Gerhold51@yahoo.com",
+      firstName: "Janis",
+      lastName: "Vandervort",
+      companyName: "Glover - Hermiston",
+    },
+  ];
+
+  const columns = [
+    {
+      id: "id",
+      width: 80,
+    },
+    {
+      id: "city",
+      width: 100,
+      header: "City",
+      footer: "City",
+    },
+    {
+      id: "firstName",
+      header: "First Name",
+      footer: "First Name",
+      width: 150,
+    },
+    {
+      id: "lastName",
+      header: "Last Name",
+      footer: "Last Name",
+      width: 150,
+    },
+    {
+      id: "email",
+      header: "Email",
+      footer: "Email",
+    },
+    {
+      id: "companyName",
+      header: "Company",
+      footer: "Company",
+    },
+  ];
 </script>
 
 <main class="container">
@@ -32,125 +88,125 @@
     <button type="submit">Greet</button>
   </form>
   <p>{greetMsg}</p>
+  <Grid {data} {columns} />
 </main>
 
 <style>
-.logo.vite:hover {
-  filter: drop-shadow(0 0 2em #747bff);
-}
+  .logo.vite:hover {
+    filter: drop-shadow(0 0 2em #747bff);
+  }
 
-.logo.svelte-kit:hover {
-  filter: drop-shadow(0 0 2em #F9FFFC);
-}
+  .logo.svelte-kit:hover {
+    filter: drop-shadow(0 0 2em #f9fffc);
+  }
 
-:root {
-  font-family: Inter, Avenir, Helvetica, Arial, sans-serif;
-  font-size: 16px;
-  line-height: 24px;
-  font-weight: 400;
-
-  color: #0f0f0f;
-  background-color: #f6f6f6;
-
-  font-synthesis: none;
-  text-rendering: optimizeLegibility;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-text-size-adjust: 100%;
-}
-
-.container {
-  margin: 0;
-  padding-top: 10vh;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  text-align: center;
-}
-
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: 0.75s;
-}
-
-.logo.tauri:hover {
-  filter: drop-shadow(0 0 2em #24c8db);
-}
-
-.row {
-  display: flex;
-  justify-content: center;
-}
-
-a {
-  font-weight: 500;
-  color: #646cff;
-  text-decoration: inherit;
-}
-
-a:hover {
-  color: #535bf2;
-}
-
-h1 {
-  text-align: center;
-}
-
-input,
-button {
-  border-radius: 8px;
-  border: 1px solid transparent;
-  padding: 0.6em 1.2em;
-  font-size: 1em;
-  font-weight: 500;
-  font-family: inherit;
-  color: #0f0f0f;
-  background-color: #ffffff;
-  transition: border-color 0.25s;
-  box-shadow: 0 2px 2px rgba(0, 0, 0, 0.2);
-}
-
-button {
-  cursor: pointer;
-}
-
-button:hover {
-  border-color: #396cd8;
-}
-button:active {
-  border-color: #396cd8;
-  background-color: #e8e8e8;
-}
-
-input,
-button {
-  outline: none;
-}
-
-#greet-input {
-  margin-right: 5px;
-}
-
-@media (prefers-color-scheme: dark) {
   :root {
-    color: #f6f6f6;
-    background-color: #2f2f2f;
+    font-family: Inter, Avenir, Helvetica, Arial, sans-serif;
+    font-size: 16px;
+    line-height: 24px;
+    font-weight: 400;
+
+    color: #0f0f0f;
+    background-color: #f6f6f6;
+
+    font-synthesis: none;
+    text-rendering: optimizeLegibility;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-text-size-adjust: 100%;
+  }
+
+  .container {
+    margin: 0;
+    padding-top: 10vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    text-align: center;
+  }
+
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: 0.75s;
+  }
+
+  .logo.tauri:hover {
+    filter: drop-shadow(0 0 2em #24c8db);
+  }
+
+  .row {
+    display: flex;
+    justify-content: center;
+  }
+
+  a {
+    font-weight: 500;
+    color: #646cff;
+    text-decoration: inherit;
   }
 
   a:hover {
-    color: #24c8db;
+    color: #535bf2;
+  }
+
+  h1 {
+    text-align: center;
   }
 
   input,
   button {
-    color: #ffffff;
-    background-color: #0f0f0f98;
+    border-radius: 8px;
+    border: 1px solid transparent;
+    padding: 0.6em 1.2em;
+    font-size: 1em;
+    font-weight: 500;
+    font-family: inherit;
+    color: #0f0f0f;
+    background-color: #ffffff;
+    transition: border-color 0.25s;
+    box-shadow: 0 2px 2px rgba(0, 0, 0, 0.2);
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  button:hover {
+    border-color: #396cd8;
   }
   button:active {
-    background-color: #0f0f0f69;
+    border-color: #396cd8;
+    background-color: #e8e8e8;
   }
-}
 
+  input,
+  button {
+    outline: none;
+  }
+
+  #greet-input {
+    margin-right: 5px;
+  }
+
+  @media (prefers-color-scheme: dark) {
+    :root {
+      color: #f6f6f6;
+      background-color: #2f2f2f;
+    }
+
+    a:hover {
+      color: #24c8db;
+    }
+
+    input,
+    button {
+      color: #ffffff;
+      background-color: #0f0f0f98;
+    }
+    button:active {
+      background-color: #0f0f0f69;
+    }
+  }
 </style>
